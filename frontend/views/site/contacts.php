fe0759047cfe8018a8e2aac926dbb33e5afba4d0
kirill <apuc06@mail.ru>
2016-10-21 14:28:20 +0300

    contacts fix

--- frontend/views/site/contacts.php
+++ frontend/views/site/contacts.php
@@ -5,7 +5,11 @@
  * Date: 21.10.2016
  * Time: 13:17
  */
+$this->title = "Контакты";
 ?>
 
 
-<div class="cont-info"><p>Портал города Донецка <a href="mailto:da.info.pro@gmail.com">da.info.pro@gmail.com</a>,<br> колл центр - +38(099)671-71-72, +38(071)310-09-54</p></div>
+<div class="cont-info"><p>г. Донецка
+        <br><a href="mailto:da.info.pro@gmail.com">da.info.pro@gmail.com</a>,
+        <br> колл центр - +38(099)671-71-72, +38(071)310-09-54</p>
+</div>
