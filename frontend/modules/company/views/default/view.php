7bb46d9048d8991501f6174110ec2243c672e07f
Anton <waryatav@gmail.com>
2016-11-10 16:25:39 +0300

    share fix

--- frontend/modules/company/views/default/view.php
+++ frontend/modules/company/views/default/view.php
@@ -9,16 +9,17 @@
 use common\classes\DateFunctions;
 
 $this->title = $company->meta_title;
-$this->registerMetaTag([
-    'name' => 'description',
+$this->registerMetaTag( [
+    'name'    => 'description',
     'content' => $company->meta_descr,
-]);
+] );
 ?>
 <div class="shape">
     <img src="/theme/portal-donbassa/img/shape-line.png" alt="">
 </div>
 <div class="news__post">
-    <span class="date-news__post"><?= date('d', $company->dt_add) ?> <?= DateFunctions::getMonthShortName(date('m', $company->dt_add)) ?> <?= date('H:i', $company->dt_add) ?></span>
+    <span
+        class="date-news__post"><?= date( 'd', $company->dt_add ) ?> <?= DateFunctions::getMonthShortName( date( 'm', $company->dt_add ) ) ?> <?= date( 'H:i', $company->dt_add ) ?></span>
     <h2><?= $company->name ?></h2>
     <a href="" class="view-img"><img src="<?= $company->photo ?>" alt=""></a>
     <p><b>Тел.:</b> <?= $company->phone ?></p>
@@ -28,29 +29,37 @@ $this->registerMetaTag([
 </div>
 <div class="post-nav">
     <span><a href=""><i class="fa fa-eye" aria-hidden="true"></i> <?= $company->views ?></a></span>
-    <?php if (!empty($company->tags)): ?>
+    <?php if ( ! empty( $company->tags ) ): ?>
         <span>Теги: <?= $company->tags ?></span>
     <?php endif ?>
-    <span>Поделись <a onclick="Share.twitter('<?= \yii\helpers\Url::to() ?>','<?= $company->name ?>')" href=""
+    <?php
+    $company_descr = strip_tags( $company->descr );
+    $company_descr = preg_replace( "/\s{2,}/", " ", $company_descr );
+    $company_name  = strip_tags( $company->name );
+    $company_name  = preg_replace( "/\s{2,}/", " ", $company_name );; ?>
+    <span>Поделись <a onclick="Share.twitter('<?=
+        \yii\helpers\Url::to() ?>',
+            '<?= $company_name ?>')" href=""
                       class="soc-icon">
             <img class="twi" src="/theme/portal-donbassa/img/twi.png" alt="">
         </a>
         <a onclick="Share.facebook(
             '<?= \yii\helpers\Url::to() ?>',
-            '<?= $company->name; ?>')" href="" class="soc-icon">
+            '<?= $company_name; ?>',
+            '<?= $company->photo?>')" href="" class="soc-icon">
             <img class="fb" src="/theme/portal-donbassa/img/fb.png" alt="">
         </a>
         <a onclick="Share.vkontakte(
             '<?= \yii\helpers\Url::to() ?>',
-            '<?= $company->name; ?>',
+            '<?= $company_name; ?>',
             '<?= $company->photo; ?>',
-            ''
+            '<?= $company_descr; ?>'
             )" href="" class="soc-icon">
             <img class="vk" src="/theme/portal-donbassa/img/vk.png" alt="">
         </a>
         <a onclick="Share.odnoklassniki(
             '<?= \yii\helpers\Url::to() ?>',
-            '<?= $company->name; ?>'
+            '<?= $company_name; ?>'
             )" href="" class="soc-icon">
             <img class="ok" src="/theme/portal-donbassa/img/ok.png" alt="">
         </a>
