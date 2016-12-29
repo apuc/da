e0d19029a6dddac76328ac41f01c054dda3875ec
kirill <apuc06@mail.ru>
2016-12-29 13:57:06 +0300

    delete phones

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -162,7 +162,7 @@ AppAsset::register($this);
             <!--<li><a href="" class="circle"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
             <li><a href="" class="circle"><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>-->
         </ul>
-        <div class="info"><p><a href="mailto:da.info.pro@gmail.com">da.info.pro@gmail.com</a>,<br> колл центр - +38(099)671-71-72, +38(071)310-09-54</p></div>
+        <div class="info"><p><a href="mailto:da.info.pro@gmail.com">da.info.pro@gmail.com</a>,<br> колл центр - +38(099)671-71-72</p></div>
         <!--        <div class="footer-copyright-cont">-->
         <!--            <p class="footer-copyright">2017 © da-info.pro- Сайт города Донецка</p>-->
         <!--            <p class="footer-copyright">info@da-info, (071) 210-80-54, (093) 998-49-04</p>-->
