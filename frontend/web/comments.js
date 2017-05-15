07a6eed05c1e556bcaa93f63ff27214856931f58
apuc <apuc06@mail.ru>
2017-05-15 09:39:04 +0300

    News rubric slider fix

---
+++ frontend/web/comments.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+
+    $(document).on('click', '.add-comment', function () {
+        $('#modal-add-comment-submit').attr('data-post-type', $(this).attr('data-post-type'));
+        $('#modal-add-comment-submit').attr('data-post-id', $(this).attr('data-post-id'));
+        $('#modal-add-comment-submit').attr('data-parent-id', $(this).attr('data-parent-id'));
+    })
+
+    $(document).on('click', '#modal-add-comment-submit', function () {
+
+        var comment = $('#comment').val();
+        if (comment != '') {
+            $.ajax({
+                url: '/ajax/ajax/add-comment',
+                type: "POST",
+                data: {
+                    _csrf: $('meta[name=csrf-token]').attr("content"),
+                    comment: comment,
+                    post_type: $(this).attr('data-post-type'),
+                    post_id: $(this).attr('data-post-id'),
+                    parent_id: $(this).attr('data-parent-id'),
+                },
+                success: function (data) {
+                    window.location.reload();
+                }
+            });
+        }
+
+        return false;
+    })
+
+})
