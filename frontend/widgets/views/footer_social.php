547ddb2a5c8596a7da81d8ba8e760c9809d24dec
kirill <apuc06@mail.ru>
2017-06-07 17:07:07 +0300

    fix footer

--- frontend/widgets/views/footer_social.php
+++ frontend/widgets/views/footer_social.php
@@ -11,8 +11,8 @@
 <div class="footer__links">
     <a href="https://ok.ru/da...infor"><i class="fa fa-odnoklassniki" aria-hidden="true"></i></a>
     <a href="https://vk.com/da_info_pro"><i class="fa fa-vk" aria-hidden="true"></i></a>
-    <a href="www.facebook.com/da.info.pro/"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+    <a href="https://www.facebook.com/da.info.pro/"><i class="fa fa-facebook" aria-hidden="true"></i></a>
     <a href="https://twitter.com/DA_info_pro"><i class="fa fa-twitter" aria-hidden="true"></i></a>
     <a href="https://www.instagram.com/da.info.pro/"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-    <a href="https://ok.ru/da...infor"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
+    <a href="https://plus.google.com/u/0/communities/115639152242515279745"><i class="fa fa-google-plus" aria-hidden="true"></i></a>
 </div>
