ceda079696cb54848e4f3dcc969d0662a7986f26
Korol Dima <korol_dima@list.ru>
2017-06-12 14:14:15 +0300

    board

---
+++ frontend/modules/board/Board.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace frontend\modules\board;
+
+/**
+ * board module definition class
+ */
+class Board extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'frontend\modules\board\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
