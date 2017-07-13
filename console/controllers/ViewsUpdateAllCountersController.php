e5bafacf28cc881716679d1de0467f153776dcda
Korol Dima <korol_dima@list.ru>
2017-07-13 17:45:23 +0300

    update count views news and company

--- console/controllers/ViewsUpdateAllCountersController.php
+++ console/controllers/ViewsUpdateAllCountersController.php
@@ -7,11 +7,42 @@
  */
 
 namespace console\controllers;
+use common\classes\Debug;
+use common\models\db\Company;
+use common\models\db\News;
 use yii\console\Controller;
+use yii\helpers\ArrayHelper;
+
 class ViewsUpdateAllCountersController extends Controller
 {
     public function actionIndex()
     {
-        echo 'test';
+        $this->countNews();
+        $this->countCompany();
     }
+
+    protected function countNews()
+    {
+        $news = News::find()
+            ->where(['status' => 0])
+            ->andWhere(['>=', 'dt_public', time() - 2592000*3])
+            ->all();
+
+        foreach ($news as $item){
+            $randNumber = rand(30,100);
+            News::updateAllCounters(['views' => $randNumber], ['id' => $item->id]);
+            echo 'ID новсти - ' . $item->id .  ' Увеличено на ' . $randNumber . "\n";
+        }
+    }
+
+    protected function countCompany()
+    {
+        $company = Company::find()->where(['status' => 0])->all();
+        foreach ($company as $item){
+            $randNumber = rand(10,50);
+            Company::updateAllCounters(['views' => $randNumber], ['id' => $item->id]);
+            echo 'ID компании - ' . $item->id .  ' Увеличено на ' . $randNumber . "\n";
+        }
+    }
+
 }
