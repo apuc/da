57a8d9cc303600751035126fd4d7c4c9fd56839d
Korol Dima <korol_dima@list.ru>
2017-07-13 17:00:15 +0300

    update count views news and company

---
+++ console/controllers/ViewsUpdateAllCountersController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 7
+ * Date: 13.07.2017
+ * Time: 16:53
+ */
+
+namespace console\controllers;
+use yii\console\Controller;
+class ViewsUpdateAllCountersController extends Controller
+{
+    public function actionIndex()
+    {
+        echo 'test';
+    }
+}
