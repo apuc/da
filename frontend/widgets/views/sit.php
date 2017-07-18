cd3a48b9ded092215b09cf23a946e107a79774ba
Korol Dima <korol_dima@list.ru>
2017-07-18 11:29:49 +0300

    fix title

--- frontend/widgets/views/sit.php
+++ frontend/widgets/views/sit.php
@@ -12,7 +12,7 @@
 <div class="home-content__wrap_checkpoint">
     <span class="color-zebra"></span>
     <div class="title">
-        <h2>Ситауция на блокпостах</h2>
+        <h2>Ситуация на блокпостах и КПП</h2>
         <!--<a href="">Все пункты пропуска<span class="rect-icon"></span></a>-->
     </div>
 
