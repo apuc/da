817c8b2aa06f5cc14afa7c128e83db02d29fb470
Korol Dima <korol_dima@list.ru>
2017-07-27 16:36:45 +0300

    fix share site

--- frontend/widgets/views/share-news.php
+++ frontend/widgets/views/share-news.php
@@ -1,42 +1,42 @@
 <?php
 ?>
-<div class="social-wrapper">
+<!--<div class="social-wrapper">
     <a onclick="Share.vkontakte(
-            '<?= $options['url'] ?>',
-            '<?= $options['title'] ?>',
-            '<?= $options['image']; ?>',
-            '<?= $options['description']; ?>'
+            '<?/*= $options['url'] */?>',
+            '<?/*= $options['title'] */?>',
+            '<?/*= $options['image']; */?>',
+            '<?/*= $options['description']; */?>'
             ); return false;"
        href="#" class="social-wrap__item vk">
         <img src="/theme/portal-donbassa/img/soc/vk.png" alt="vk">
     </a>
     <a onclick="Share.facebook(
-            '<?= $options['url'] ?>',
-            '<?= $options['title']; ?>',
-            '<?= $options['image']; ?>',
-            '<?= $options['description']; ?>')"
+            '<?/*= $options['url'] */?>',
+            '<?/*= $options['title']; */?>',
+            '<?/*= $options['image']; */?>',
+            '<?/*= $options['description']; */?>')"
        href="#" class="social-wrap__item fb">
         <img src="/theme/portal-donbassa/img/soc/fb.png" alt="fb">
     </a>
     <a onclick="Share.odnoklassniki(
-            '<?= $options['url'] ?>',
-            '<?= $options['title']; ?>'
+            '<?/*= $options['url'] */?>',
+            '<?/*= $options['title']; */?>'
             )" href="#" class="social-wrap__item ok">
         <img src="/theme/portal-donbassa/img/soc/ok-icon.png" alt="ok">
     </a>
-    <!--<a href="#" target="_blank" class="social-wrap__item insta">-->
-    <!--    <img src="/theme/portal-donbassa/img/soc/insta-icon.png" alt="instagramm">-->
-    <!--</a>-->
-    <!--<a href="#" target="_blank" class="social-wrap__item google">-->
-    <!--    <img src="/theme/portal-donbassa/img/soc/google-icon.png" alt="google">-->
-    <!--</a>-->
+
     <a onclick="Share.twitter(
-            '<?= $options['url'] ?>',
-            '<?= $options['title'] ?>',
-            '<?= $options['image']; ?>',
-            '<?= $options['description']; ?>'
+            '<?/*= $options['url'] */?>',
+            '<?/*= $options['title'] */?>',
+            '<?/*= $options['image']; */?>',
+            '<?/*= $options['description']; */?>'
         )" href="#" class="social-wrap__item twitter">
         <img src="/theme/portal-donbassa/img/soc/twi-icon.png" alt="twitter">
     </a>
-</div>
+</div>-->
 
+<div class="social-wrapper">
+    <script src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script>
+    <script src="//yastatic.net/share2/share.js"></script>
+    <div class="ya-share2" data-bare data-services="vkontakte,facebook,odnoklassniki,gplus,pinterest,twitter,linkedin,lj,telegram" data-image="http://da-info.pro/images/soc/pinterest_social.png"></div>
+</div>
