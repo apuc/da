f98dcca260072048fd1d6590b8785d5303be45a1
Korol Dima <korol_dima@list.ru>
2017-08-04 10:46:00 +0300

    update view slider company main page

--- frontend/widgets/views/company_main.php
+++ frontend/widgets/views/company_main.php
@@ -9,60 +9,6 @@
 use yii\helpers\Url;
 
 ?>
-<!--<section class="company">
-
-    <div class="container">
-
-        <h3 class="main-title">предприятия</h3>
-        <span class="separator"></span>
-
-        <a href="#" class="company__trigger">предприятия</a>
-
-        <div class="company__box">
-            <?php /*foreach ($companies as $k => $company): */?>
-                <?php /*if ($k === 1 || $k === 7 || $k === 8): */?>
-                    <a  href="<?/*= Url::to(['/company/company/view', 'slug' => $company->slug]) */?>" class="item-large">
-                        <div class="thumb">
-                            <img src="<?/*= \common\models\UploadPhoto::getImageOrNoImage($company->photo) */?>" alt="">
-                        </div>
-                        <div class="content">
-                            <h4><?/*= $company->name */?></h4>
-                            <!-- <p>Донецк Адрес: Пухова 31б </p>
-                            <p>Телефон: 0667778540</p>
-                        </div>
-                        <div class="content-hover">
-                            <h4><?/*= $company->name */?></h4>
-                            <p><?/*= $company->address */?></p>
-                            <p><?/*= $company->phone */?></p>
-                        </div>
-                    </a>
-                <?php /*else: */?>
-                    <a  href="<?/*= Url::to(['/company/company/view', 'slug' => $company->slug]) */?>"  class="item-small">
-                        <div class="thumb">
-                            <img src="<?/*= \common\models\UploadPhoto::getImageOrNoImage($company->photo) */?>" alt="">
-                        </div>
-                        <div class="content">
-                            <h4><?/*= $company->name */?></h4>
-                            <!-- <p>Донецк Адрес: Пухова 31б </p> -->
-                            <!-- <p>Телефон: 0667778540</p>
-                        </div>
-                        <div class="content-hover">
-                            <h4><?/*= $company->name */?></h4>
-                            <p><?/*= $company->address */?> </p>
-                            <p><?/*= $company->phone */?></p>
-                        </div>
-                    </a>
-                <?php /*endif; */?>
-
-            <?php /*endforeach; */?>
-        </div>
-
-        <a href="<?/*= Url::to(['/company/company']) */?>" class="more">посмотреть больше <i class="fa fa-chevron-right" aria-hidden="true"></i></a>
-
-    </div>
-
-</section>-->
-
 <section class="company-slider">
 
     <div class="container">
@@ -80,7 +26,8 @@ use yii\helpers\Url;
                     <div class="company-slider__box_item">
                 <?php endif;?>
                     <?php if($count == 1): ?>
-                        <a href="<?= Url::to(['/company/company/view', 'slug' => $company->slug]) ?>" class="business__big-item favorite">
+                        <a href="<?= Url::to(['/company/company/view', 'slug' => $company->slug]) ?>"
+                           class="business__big-item <?= ($company->recommended == 1) ? 'favorite' : ''?>">
 
                             <div class="recommend">
                                 <span class="recommend__star"></span>
@@ -114,7 +61,8 @@ use yii\helpers\Url;
 
                         </a>
                     <?php else: ?>
-                        <a href="<?= Url::to(['/company/company/view', 'slug' => $company->slug]) ?>" class="business__sm-item">
+                        <a href="<?= Url::to(['/company/company/view', 'slug' => $company->slug]) ?>"
+                           class="business__sm-item <?= ($company->recommended == 1) ? 'favorite' : ''?>">
 
                             <div class="recommend">
                                 <span class="recommend__star"></span>
@@ -157,8 +105,8 @@ use yii\helpers\Url;
             <?php endforeach;?>
         </div>
 
-        <a href="<?= Url::to(['/company/company']) ?>" class="more">посмотреть больше <i class="fa fa-chevron-right" aria-hidden="true"></i></a>
+
 
     </div>
-
+        <a href="<?= Url::to(['/company/company']) ?>" class="more">посмотреть больше <i class="fa fa-chevron-right" aria-hidden="true"></i></a>
 </section>
