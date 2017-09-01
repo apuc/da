e45f204086ad870bf0e6db3abc89c4f430b4dd42
king199025 <korol_dima@list.ru>
2017-09-01 15:10:37 +0300

    update css

--- frontend/modules/personal_area/views/user-ads/index.php
+++ frontend/modules/personal_area/views/user-ads/index.php
@@ -6,4 +6,100 @@
  * Time: 14:26
  */
 ?>
-21312312
+<div class="cabinet__inner-box">
+
+    <h3>Мои объявления</h3>
+    <a href="<?= \yii\helpers\Url::to(['/board/default/create'])?>" class="cabinet__inner-box--add">
+        Добавить предприятие
+        <span>
+            <img src="/theme/portal-donbassa/img/icons/add-pkg-icon.png" alt="">
+        </span>
+    </a>
+    <div class="cabinet__company-box">
+        <div class="cabinet__company-box--img">
+            <img src="/img/cabinet/flat.png" alt="">
+        </div>
+        <div class="cabinet__company-box--content">
+            <div class="cabinet__company-box--data">
+                <span>Сегодня 20:00</span>
+                <span>Квартиры</span>
+            </div>
+            <div class="cabinet__company-box--text">
+                <span><a href="#">2-x комнатная квартира, 63 м<sup>2</sup>, 2/5 этаж</a></span>
+                <p>Речной вокзал 1.9 км, ул. Дыбенко д.16 корп.1
+                <p>
+                    <span>2 500 000</span>
+            </div>
+        </div>
+        <div class="cabinet__company-box--edit">
+            <a href="#"><img src="/img/icons/edit-img.png"><span>редактировать</span></a>
+            <a href="#"><img src="/img/icons/delete-img.png"><span>удалить</span></a>
+        </div>
+        <div class="cabinet__company-box--button">
+            <a href="#" class="show-more">Обновить объявление</a>
+        </div>
+    </div>
+    <div class="cabinet__company-box">
+        <div class="cabinet__company-box--img">
+            <img src="/img/cabinet/flat.png" alt="">
+        </div>
+        <div class="cabinet__company-box--content">
+            <div class="cabinet__company-box--data">
+                <span>Сегодня 20:00</span>
+                <span>Квартиры</span>
+            </div>
+            <div class="cabinet__company-box--text">
+                <span><a href="#">2-x комнатная квартира, 63 м<sup>2</sup>, 2/5 этаж</a></span>
+                <p>Речной вокзал 1.9 км, ул. Дыбенко д.16 корп.1
+                <p>
+                    <span>2 500 000</span>
+            </div>
+        </div>
+        <div class="cabinet__company-box--edit">
+            <a href="#"><img src="/img/icons/edit-img.png"><span>редактировать</span></a>
+            <a href="#"><img src="/img/icons/delete-img.png"><span>удалить</span></a>
+        </div>
+        <div class="cabinet__company-box--mess">
+            <span>Объявление</span>
+            <span>на модерации</span>
+            <p>Ваше объявление будет опубликовано как только пройдет модерацию</p>
+        </div>
+    </div>
+    <div class="cabinet__company-box">
+        <div class="cabinet__company-box--img">
+            <img src="/img/cabinet/flat.png" alt="">
+        </div>
+        <div class="cabinet__company-box--content">
+            <div class="cabinet__company-box--data">
+                <span>Сегодня 20:00</span>
+                <span>Квартиры</span>
+            </div>
+            <div class="cabinet__company-box--text">
+                <span><a href="#">2-x комнатная квартира, 63 м<sup>2</sup>, 2/5 этаж</a></span>
+                <p>Речной вокзал 1.9 км, ул. Дыбенко д.16 корп.1
+                <p>
+                    <span>2 500 000</span>
+            </div>
+        </div>
+        <div class="cabinet__company-box--edit">
+            <a href="#"><img src="/img/icons/edit-img.png"><span>редактировать</span></a>
+            <a href="#"><img src="/img/icons/delete-img.png"><span>удалить</span></a>
+        </div>
+        <div class="cabinet__company-box--mess2">
+            <span>ОПУБЛИКОВАНО</span>
+            <p>Ваше объявление уже размещено на нашем сайте а так же на доске объявлений
+                <a>RUB-ON.RU</a>
+            </p>
+        </div>
+    </div>
+
+
+    <!--<div id="w0" class="list-view">-->
+    <!--<div>-->
+    <!--<div class="cabinet__add-element">-->
+    <!--<p>Раздел пока пуст</p>-->
+    <!--<a href="/news/news/create" class="show-more">добавить</a>-->
+    <!--</div>-->
+    <!--</div>-->
+    <!--</div>-->
+</div>
