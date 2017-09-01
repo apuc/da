3a818ca6673ea1d6f3b9cf8118e10a15854cff78
king199025 <korol_dima@list.ru>
2017-09-01 14:38:13 +0300

    fix board rule

--- frontend/modules/personal_area/controllers/UserAdsController.php
+++ frontend/modules/personal_area/controllers/UserAdsController.php
@@ -8,7 +8,30 @@
 
 namespace frontend\modules\personal_area\controllers;
 
-class UserAdsController
-{
+use yii\base\Controller;
+use yii\filters\AccessControl;
 
+class UserAdsController extends Controller
+{
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ];
+    }
+    public $layout = 'personal_area';
+
+    public function actionIndex()
+    {
+
+        return $this->render('index');
+    }
 }
---
+++ frontend/modules/personal_area/views/user-ads/index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: king
+ * Date: 01.09.17
+ * Time: 14:26
+ */
+?>
+21312312
