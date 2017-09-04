00c944e651320cfb6666debfd65d549b789c1b59
king199025 <korol_dima@list.ru>
2017-09-04 10:33:08 +0300

    personal area fix likes

--- frontend/modules/personal_area/views/user-likes/_list.php
+++ frontend/modules/personal_area/views/user-likes/_list.php
@@ -1,17 +1,38 @@
 <?php
 use common\classes\WordFunctions;
+use common\models\db\VkStream;
+
+//\common\classes\Debug::prn($model);
 
 if($model['post_type'] == 'news') {
     $post = \common\models\db\News::find()->where(['id' => $model['post_id']])->one();
     $url = \yii\helpers\Url::to(['/news/default/view', 'slug' => $post->slug]);
     $name = 'Новости';
     $time = WordFunctions::FullEventDate($post->dt_public);
+    $imgUrl = \common\models\UploadPhoto::getImageOrNoImage($post->photo);
 }
 
 if($model['post_type'] == 'poster') {
     $post = \common\models\db\Poster::find()->where(['id' => $model['post_id']])->one();
     $url = \yii\helpers\Url::to(['/poster/default/view', 'slug' => $post->slug]);
     $name = 'Афиша';
+    $imgUrl = \common\models\UploadPhoto::getImageOrNoImage($post->photo);
+}
+
+if($model['post_type'] == 'stream') {
+    $post = VkStream::find()->with('photo', 'comments', 'author', 'group')
+        ->where(['id' => $model['post_id']])
+        ->one();
+
+    if(isset($post['photo'][0])){
+        $imgUrl = $post['photo'][0]->photo_130;
+    }else{
+        $imgUrl = '/theme/portal-donbassa/img/no-image.png';
+    }
+
+    $url = \yii\helpers\Url::to(['/stream/default/view', 'slug' => $post->slug]);
+
+    $name = 'В соцсетях';
 }
 
 
@@ -21,7 +42,7 @@ if($model['post_type'] == 'poster') {
 <a href="<?= $url?>" class="cabinet__like-block--section"><?= $name; ?></a>
 
 <a href="<?= $url?>" class="cabinet__like-block--photo">
-    <img src="<?= \common\models\UploadPhoto::getImageOrNoImage($post->photo); ?>" alt="">
+    <img src="<?= $imgUrl; ?>" alt="">
 </a>
 
 <a href="<?= $url?>" class="cabinet__like-block--descr"><?= $post->title; ?></a>
