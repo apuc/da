96c91ba75029919fdc15c806749cbadb37f63fd9
king199025 <korol_dima@list.ru>
2017-09-14 11:56:39 +0300

    view ads

--- backend/modules/board/controllers/DefaultController.php
+++ backend/modules/board/controllers/DefaultController.php
@@ -2,6 +2,10 @@
 
 namespace backend\modules\board\controllers;
 
+use common\behaviors\AccessSecure;
+use common\classes\Debug;
+use Yii;
+use yii\data\Pagination;
 use yii\web\Controller;
 
 /**
@@ -9,12 +13,84 @@ use yii\web\Controller;
  */
 class DefaultController extends Controller
 {
+    public $siteApi;
+    public $apiKey;
+
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessSecure::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    public function beforeAction($action)
+    {
+        $this->siteApi = Yii::$app->params['site-api'];
+        $this->apiKey = Yii::$app->params['api-key'];
+        return parent::beforeAction($action);
+    }
+
     /**
      * Renders the index view for the module
      * @return string
      */
     public function actionIndex()
     {
-        return $this->render('index');
+        $rez = file_get_contents($this->siteApi . '/ads/ads-list-all?limit=20&expand=categoryAds&page=' . Yii::$app->request->get('page',1) . '&api_key=' . $this->apiKey);
+        //$rez = file_get_contents($this->siteApi . '/ads/index?limit=10&expand=adsImgs,adsFieldsValues,city,region,categoryAds&page=' . Yii::$app->request->get('page',1));
+
+
+        $rez = json_decode($rez);
+
+        if(!isset($rez->_meta->totalCount)){
+            echo $rez;
+        }else{
+            $pagination = new Pagination([
+                'defaultPageSize' => 10,
+                'totalCount' => $rez->_meta->totalCount,
+                'pageSizeParam' => false,
+            ]);
+            return $this->render('index',
+                [
+                    'ads' => $rez->ads,
+                    'pagination' => $pagination,
+                ]
+            );
+        }
+    }
+
+    public function actionView($id)
+    {
+        $ads = file_get_contents($this->siteApi . '/ads/' . $id . '?expand=adsImgs,adsFieldsValues' . '&api_key=' . $this->apiKey);
+
+        $ads = json_decode($ads);
+        if(!isset($ads->title)){
+            echo $ads;
+        }
+        else {
+            return $this->render('view',
+                [
+                    'ads' => $ads,
+                ]
+            );
+        }
+    }
+
+    public function actionEditStatus($status, $id)
+    {
+        Debug::prn($status);
+        Debug::prn($id);
     }
 }
--- backend/modules/board/views/default/index.php
+++ backend/modules/board/views/default/index.php
@@ -1,12 +1,64 @@
-<div class="board-default-index">
-    <h1><?= $this->context->action->uniqueId ?></h1>
-    <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
-    </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
-</div>
+<?php
+/**
+* @var $ads
+*/
+
+use yii\widgets\LinkPager;
+
+$this->title = 'Объявления';
+?>
+
+<section class="content">
+    <div class="stock-index">
+
+        <h1><?= $this->title; ?></h1>
+
+        <div id="w0" class="grid-view">
+            <table class="table table-striped table-bordered">
+                <thead>
+                    <tr>
+                        <th>Заголовок</th>
+                        <th>Статус</th>
+                    </tr>
+                </thead>
+                <tbody>
+                <?php foreach ($ads as $item): ?>
+                    <tr>
+                        <td><?= $item->title; ?></td>
+                        <td>
+                            <?php
+                                switch ($item->status){
+                                    case 1: echo 'На модерации';break;
+                                    case 2: echo 'Опубликованно';break;
+                                    case 3: echo 'Удалено';break;
+                                    case 4: echo 'VIP';break;
+                                    case 5: echo 'Снято с публикации';break;
+                                    case 6: echo 'Снято с публикации (модератор)';break;
+
+                                }
+                            ?>
+                        </td>
+                        <td><a href="<?= \yii\helpers\Url::to(['view', 'id' => $item->id])?>">Посмотреть</a></td>
+                    </tr>
+                <?php endforeach; ?>
+
+                </tbody>
+            </table>
+            <div class="pagination">
+                <?= LinkPager::widget(
+                    [
+                        'pagination' => $pagination,
+                        'options' => [
+                            'class' => '',
+                        ],
+                        'prevPageCssClass' => 'pagination-prew',
+                        'nextPageCssClass' => 'pagination-next',
+                        'prevPageLabel' => '',
+                        'nextPageLabel' => '',
+                        'activePageCssClass' => 'active',
+
+                    ]) ?>
+            </div>
+        </div>
+    </div>
+</section>
