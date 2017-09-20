13c501cc49a5ed1f9a8980415372cded7fe1f373
king199025 <korol_dima@list.ru>
2017-09-20 16:11:02 +0300

    redirec url back login board and news

--- frontend/modules/board/controllers/DefaultController.php
+++ frontend/modules/board/controllers/DefaultController.php
@@ -20,6 +20,20 @@ class DefaultController extends Controller
     public $siteApi;
     public $apiKey;
 
+    public function init()
+    {
+        $this->on('beforeAction', function ($event) {
+
+            // запоминаем страницу неавторизованного пользователя, чтобы потом отредиректить его обратно с помощью  goBack()
+            if (Yii::$app->getUser()->isGuest) {
+                $request = Yii::$app->getRequest();
+                // исключаем страницу авторизации или ajax-запросы
+                if (!($request->getIsAjax() || strpos($request->getUrl(), 'login') !== false)) {
+                    Yii::$app->getUser()->setReturnUrl($request->getUrl());
+                }
+            }
+        });
+    }
 
     /**
      * @inheritdoc
--- frontend/modules/news/controllers/DefaultController.php
+++ frontend/modules/news/controllers/DefaultController.php
@@ -21,6 +21,20 @@ class DefaultController extends Controller
 {
     public $layout = 'portal_page';
 
+    public function init()
+    {
+        $this->on('beforeAction', function ($event) {
+
+            // запоминаем страницу неавторизованного пользователя, чтобы потом отредиректить его обратно с помощью  goBack()
+            if (Yii::$app->getUser()->isGuest) {
+                $request = Yii::$app->getRequest();
+                // исключаем страницу авторизации или ajax-запросы
+                if (!($request->getIsAjax() || strpos($request->getUrl(), 'login') !== false)) {
+                    Yii::$app->getUser()->setReturnUrl($request->getUrl());
+                }
+            }
+        });
+    }
     /**
      * Renders the index view for the module
      * @return string
