6d4c0d3c2cdde52a26dad957113dc4d29dbcbd89
king199025 <korol_dima@list.ru>
2017-09-20 16:28:16 +0300

    redirec url back login promotion

--- frontend/modules/promotions/controllers/PromotionsController.php
+++ frontend/modules/promotions/controllers/PromotionsController.php
@@ -21,6 +21,21 @@ use yii\web\NotFoundHttpException;
 
 class PromotionsController extends Controller
 {
+    public function init()
+    {
+        $this->on('beforeAction', function ($event) {
+
+            // запоминаем страницу неавторизованного пользователя, чтобы потом отредиректить его обратно с помощью  goBack()
+            if (Yii::$app->getUser()->isGuest) {
+                $request = Yii::$app->getRequest();
+                // исключаем страницу авторизации или ajax-запросы
+                if (!($request->getIsAjax() || strpos($request->getUrl(), 'login') !== false)) {
+                    Yii::$app->getUser()->setReturnUrl($request->getUrl());
+                }
+            }
+        });
+    }
+
     public function behaviors()
     {
         return [
