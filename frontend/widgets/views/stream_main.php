2f0c3e8630247ecacdfd423667ee6d340b87d90d
king199025 <korol_dima@list.ru>
2017-10-12 09:39:30 +0300

    stream widget fix

--- frontend/widgets/views/stream_main.php
+++ frontend/widgets/views/stream_main.php
@@ -2,28 +2,17 @@
 use yii\helpers\Url;
 ?>
 
-<section class="what-say">
-
+<!-- start social_networks.html-->
+<section class="social-networks">
     <div class="container">
-
-        <h2>Тем временем в соцсетях:</h2>
-
-        <div class="what-say__servises">
-
-            <!--<a href=""><span class="comments-icon"></span>Задать свой вопрос</a>-->
-
-            <a href=""><span class="mail-icon"></span>Подписаться на эту тему</a>
-
+        <div class="social-networks__header">
+            <h1>Тем временем в соцсетях:</h1>
+            <a href="#subscribe"><span class="fa fa-envelope-o"></span>Подписаться на эту тему</a>
         </div>
-
-        <div class="what-say__wrap">
-
+        <div class="social-networks__content">
             <?php foreach ($posts as $post):?>
-
-                <a href="<?= \yii\helpers\Url::to(['/stream/default/view', 'slug' => $post->slug])?>" class="what-say__wrap_item">
-                    <!--<span class="counter">99</span>-->
-                    <div class="thumb">
-                        <!-- <span>A</span>-->
+                <a href="<?= \yii\helpers\Url::to(['/stream/default/view', 'slug' => $post->slug])?>" class="social-networks__content--item">
+                    <div class="item-photo">
                         <?php if(!empty($post->author)):?>
                             <?php if(!empty($post->author->photo)): ?>
                                 <img src="<?= $post->author->photo?>" alt="">
@@ -33,37 +22,84 @@ use yii\helpers\Url;
                             <img src="<?= $post->group->getPhoto()?>" alt="">
 
                         <?php endif;?>
-
                     </div>
-                    <div class="wrapi">
+                    <div class="item-tittle">
                         <?php if(!empty($post->author)):?>
-                            <span class="name"><?= $post->author->first_name.' '.$post->author->last_name ?></span>
+                            <h2><?= $post->author->first_name.' '.$post->author->last_name ?></h2>
 
                         <?php elseif(!empty($post->group)):?>
-                            <span class="name"><?= $post->group->name ?></span>
+                            <h2 class="name"><?= $post->group->name ?></h2>
                         <?php endif;?>
-
+                    </div>
+                    <div class="item-description">
                         <p><?= $post->title?></p>
                     </div>
-
-                    <div class="what-say__wrap--photo">
-
+                    <div class="item-img">
                         <?php if (!empty($post->photo)): ?>
                             <img src="<?= $post->photo[0]->getLargePhoto()?>" alt="">
                         <?php elseif (!empty($post->gif)): ?>
                             <img src="<?= $post->gif[0]->getLargePreview()?>" alt="">
                         <?php endif; ?>
+                    </div>
+            </a>
+            <?php endforeach; ?>
+        </div>
+    </div>
+</section>
+<!-- end social_networks.html-->
+
+<!--<section class="what-say">
+    <div class="container">
+        <h2>Тем временем в соцсетях:</h2>
+        <div class="what-say__servises">
+            <a href=""><span class="mail-icon"></span>Подписаться на эту тему</a>
+        </div>
+        <div class="what-say__wrap">
+            <?php /*foreach ($posts as $post):*/?>
+                <a href="<?/*= \yii\helpers\Url::to(['/stream/default/view', 'slug' => $post->slug])*/?>" class="what-say__wrap_item">
+
+                    <div class="thumb">
+
+                        <?php /*if(!empty($post->author)):*/?>
+                            <?php /*if(!empty($post->author->photo)): */?>
+                                <img src="<?/*= $post->author->photo*/?>" alt="">
+                            <?php /*endif; */?>
+                        <?php /*elseif(!empty($post->group)):*/?>
+
+                            <img src="<?/*= $post->group->getPhoto()*/?>" alt="">
+
+                        <?php /*endif;*/?>
+
+                    </div>
+                    <div class="wrapi">
+                        <?php /*if(!empty($post->author)):*/?>
+                            <span class="name"><?/*= $post->author->first_name.' '.$post->author->last_name */?></span>
+
+                        <?php /*elseif(!empty($post->group)):*/?>
+                            <span class="name"><?/*= $post->group->name */?></span>
+                        <?php /*endif;*/?>
+
+                        <p><?/*= $post->title*/?></p>
+                    </div>
+
+                    <div class="what-say__wrap--photo">
+
+                        <?php /*if (!empty($post->photo)): */?>
+                            <img src="<?/*= $post->photo[0]->getLargePhoto()*/?>" alt="">
+                        <?php /*elseif (!empty($post->gif)): */?>
+                            <img src="<?/*= $post->gif[0]->getLargePreview()*/?>" alt="">
+                        <?php /*endif; */?>
 
                     </div>
 
                 </a>
 
-            <?php endforeach; ?>
+            <?php /*endforeach; */?>
 
         </div>
-        <a href="<?= Url::to(['/stream']) ?>" class="more">посмотреть больше <i class="fa fa-chevron-right" aria-hidden="true"></i></a>
+        <a href="<?/*= Url::to(['/stream']) */?>" class="more">посмотреть больше <i class="fa fa-chevron-right" aria-hidden="true"></i></a>
     </div>
 
 
 
-</section>
+</section>-->
