72586f76ebf21220d2bd7dbda2eddf2649088fa8
king199025 <korol_dima@list.ru>
2017-12-14 16:50:53 +0300

    dix appasset

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -18,39 +18,39 @@ class AppAsset extends AssetBundle
     public $basePath = '@webroot';
     public $baseUrl = '@web';
     public $css = [
-        'css/site.min.css',
+        //'css/site.min.css',
         'https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
         '/theme/portal-donbassa/css/libs.min.css',
-        '/theme/portal-donbassa/css/datepicker.min.css',
-        '/theme/portal-donbassa/css/fancybox/jquery.fancybox.min.css',
-        '/theme/portal-donbassa/css/fancybox/jquery.fancybox-thumbs.min.css',
-        '/theme/portal-donbassa/css/fancybox/jquery.fancybox-buttons.min.css',
-        '/css/lightbox.min.css',
+        //'/theme/portal-donbassa/css/datepicker.min.css',
+        //'/theme/portal-donbassa/css/fancybox/jquery.fancybox.min.css',
+        //'/theme/portal-donbassa/css/fancybox/jquery.fancybox-thumbs.min.css',
+        //'/theme/portal-donbassa/css/fancybox/jquery.fancybox-buttons.min.css',
+        //'/css/lightbox.min.css',
         '/theme/portal-donbassa/css/styles.min.css',
 
     ];
     public $js = [
-        'https://api-maps.yandex.ru/2.1/?lang=ru_RU',
-        '/theme/portal-donbassa/js/countdown.min.js',
+        //'https://api-maps.yandex.ru/2.1/?lang=ru_RU',
+        //'/theme/portal-donbassa/js/countdown.min.js',
 
-        '/theme/portal-donbassa/js/map.min.js',
+        //'/theme/portal-donbassa/js/map.min.js',
         '/theme/portal-donbassa/js/libs.min.js',
         '/theme/portal-donbassa/js/owl.carousel.min.js',
-        '/theme/portal-donbassa/js/datepicker.min.js',
-        '/theme/portal-donbassa/js/jquery.fancybox.min.js',
-        '/theme/portal-donbassa/js/headhesive.min.js',
+        /* '/theme/portal-donbassa/js/datepicker.min.js',*/
+         '/theme/portal-donbassa/js/jquery.fancybox.min.js',
+         '/theme/portal-donbassa/js/headhesive.min.js',
         '/theme/portal-donbassa/js/script.min.js',
         '/theme/portal-donbassa/js/jquery.hc-sticky.min.js',
-        '/theme/portal-donbassa/js/ajax.min.js',
+        /*'/theme/portal-donbassa/js/ajax.min.js',*/
 
-        '/js/poll_ajax.min.js',
-        '/js/comments.min.js',
+        /*'/js/poll_ajax.min.js',
+        '/js/comments.min.js',*/
         '/js/share.min.js',
         '/js/main.min.js',
         '/js/main_page_ajax.min.js',
     ];
-    public $depends = [
+    /*public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap\BootstrapAsset',
-    ];
+    ];*/
 }
