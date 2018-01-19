41b250887dadc842542a648d8438da73a7cf31b8
Dmitriy <dilife90@gmail.com>
2018-01-19 15:09:46 +0300

    fix "?"

--- frontend/modules/personal_area/views/user-company/_list.php
+++ frontend/modules/personal_area/views/user-company/_list.php
@@ -86,7 +86,7 @@ if ($show) {
 
 
     array_walk($cvRegion, function (&$item) {
-        $item['name'] = is_null($item['name']) ? $item['name'] = '?' : $item['name'];
+        $item['name'] = is_null($item['name']) ? $item['name'] = 'Не определено' : $item['name'];
         $item['sum'] = (int)$item['sum'];
         $item = array_values($item);
     });
