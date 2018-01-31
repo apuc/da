b1ca45e6053b4f3919cb42cb9638d15629d60249
king199025 <korol_dima@list.ru>
2018-01-31 18:01:20 +0300

    show category shop

--- frontend/modules/shop/widgets/ShowAllShopsCategory.php
+++ frontend/modules/shop/widgets/ShowAllShopsCategory.php
@@ -8,12 +8,24 @@
 
 namespace frontend\modules\shop\widgets;
 
+use common\classes\Debug;
+use common\models\db\CategoryShop;
 use yii\base\Widget;
 
 class ShowAllShopsCategory extends Widget
 {
     public function run()
     {
+        $category = CategoryShop::find()->all();
+
+        $categoryTree = $this->categoryTree($category);
+        Debug::prn($category);
         return $this->render('all-category');
+    }
+
+    protected function categoryTree($category)
+    {
+
+        return $category;
     }
 }
