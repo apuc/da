ffe85f78f6d941e56674c45584f67d98bc67c687
Dmitriy <dilife90@gmail.com>
2018-02-01 18:00:53 +0300

    edit companyRight widget

--- frontend/widgets/views/company_right.php
+++ frontend/widgets/views/company_right.php
@@ -8,35 +8,38 @@ use yii\helpers\Url;
 ?>
 
 <div class="promotions-sidebar" id="promotion-sidebar">
-    <h3 class="main-title">Рекомендуем</h3>
-    <div class="recommended">
-        <?php foreach ($companies as $company): ?>
-            <div class="img">
-                <a href="<?= Url::to(['/company/company/view', 'slug' => $company->slug]) ?>">
+    <h3 class="main-title">рекомендуем</h3>
+    <?php foreach ($companies as $company): ?>
+        <a href="<?= Url::to(['/company/company/view', 'slug' => $company->slug]) ?>">
+            <div href="<?= Url::to(['/company/company/view', 'slug' => $company->slug]) ?>" class="company-sidebar">
+                <div class="company-sidebar__img">
                     <img src="<?= $company->photo; ?>" alt="">
-                </a>
-            </div>
-            <a href="<?= Url::to(['/company/company/view', 'slug' => $company->slug]) ?>" class="title">
-                <?= $company->name; ?>
-            </a>
-            <div class="desc">
-                <?php if (!empty($company->phone)): ?>
-                    <?php $phone = explode(' ', $company->phone) ?>
-                    <a href="<?= Url::to(['/company/company/view', 'slug' => $company->slug]) ?>">
-                        <?= isset($phone[0]) ? $phone[0] : '' ?>
-                    </a>
-                <?php elseif (!empty($company->allPhones)): ?>
-                    <?php foreach ($company->allPhones as $key => $phones): ?>
-                        <a href="<?= Url::to(['/company/company/view', 'slug' => $company->slug]) ?>">
-                            <?= $phones->phone ?>
-                        </a>
-                        <?php if ($key == 0) break; ?>
-                    <?php endforeach; ?>
-                <?php endif; ?>
-                <div>
-                    <span class="review"><?= $company->views; ?></span>
+                </div>
+                <h2 class="company-sidebar__title"><?= $company->name; ?></h2>
+                <div class="company-sidebar__desc">
+                    <div class="company-sidebar__link">
+                        <?php if (!empty($company->phone)): ?>
+                            <?php $phone = explode(' ', $company->phone) ?>
+                            <a href="tel:<?= isset($phone[0]) ? $phone[0] : '' ?>">
+                                <?= isset($phone[0]) ? $phone[0] : '' ?>
+                            </a>
+                            <a href="tel:<?= isset($phone[1]) ? $phone[1] : '' ?>">
+                                <?= isset($phone[1]) ? $phone[1] : '' ?>
+                            </a>
+                        <?php elseif (!empty($company->allPhones)): ?>
+                            <?php foreach ($company->allPhones as $key => $phones): ?>
+                                <a href="tel:<?= $phones->phone ?>">
+                                    <?= $phones->phone ?>
+                                </a>
+                                <?php if ($key == 1) break; ?>
+                            <?php endforeach; ?>
+                        <?php endif; ?>
+                    </div>
+                    <div class="company-sidebar__view">
+                        <span><?= $company->views; ?></span>
+                    </div>
                 </div>
             </div>
-        <?php endforeach; ?>
-    </div>
+        </a>
+    <?php endforeach; ?>
 </div>
