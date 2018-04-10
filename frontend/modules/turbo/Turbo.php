8bcf2e94a215963a05f5e7e2de5cf379a70183ee
kirill <apuc06@mail.ru>
2018-04-10 14:33:25 +0300

    turbo first

---
+++ frontend/modules/turbo/Turbo.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace frontend\modules\turbo;
+
+/**
+ * turbo module definition class
+ */
+class Turbo extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'frontend\modules\turbo\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
