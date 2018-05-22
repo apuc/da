db92189814e455c6f86a3ebfb7fa3addc1f138a0
IgorM0 <vvex@mail.ru>
2018-05-22 11:45:22 +0300

    fix

--- frontend/modules/shop/widgets/views/product-company.php
+++ frontend/modules/shop/widgets/views/product-company.php
@@ -13,30 +13,30 @@ if(!empty($model)):
         <div class="shop__top-sales-elements">
             <?php foreach ($model as $item): ?>
 
-                <a href="<?= \yii\helpers\Url::to(['/shop/shop/show', 'slug' => $item->product->slug])?>" class="shop__top-sales-elements--item">
+                <a href="<?= \yii\helpers\Url::to(['/shop/shop/show', 'slug' => $item->slug])?>" class="shop__top-sales-elements--item">
 
-                    <h3 class="category-name"><?= $item->product->title; ?></h3>
+                    <h3 class="category-name"><?= $item->title; ?></h3>
                     <!--<p class="category-element">iPhone 6s 16Gb</p>-->
 
                     <div class="category-photo">
                         <?php
-                        if (!empty($item->product->cover)): ?>
-                            <img src="<?= \common\models\UploadPhoto::getImageOrNoImage( $item->product->cover); ?>" alt="<?= $item->product->title; ?>">
+                        if (!empty($item->cover)): ?>
+                            <img src="<?= \common\models\UploadPhoto::getImageOrNoImage( $item->cover); ?>" alt="<?= $item->title; ?>">
                         <?php else: ?>
-                            <?php if(!empty($item->product->images[0]->img_thumb)): ?>
-                                <img src="<?= \common\models\UploadPhoto::getImageOrNoImage('/'. $item->product->images[0]->img_thumb); ?>">
+                            <?php if(!empty($item->images[0]->img_thumb)): ?>
+                                <img src="<?= \common\models\UploadPhoto::getImageOrNoImage('/'. $item->images[0]->img_thumb); ?>">
                             <?php else:?>
-                                <img src="<?= \common\models\UploadPhoto::getImageOrNoImage( $item->product->cover); ?>" alt="<?= $item->product->title; ?>">
+                                <img src="<?= \common\models\UploadPhoto::getImageOrNoImage( $item->cover); ?>" alt="<?= $item->title; ?>">
                             <?php endif; ?>
                         <?php endif; ?>
                     </div>
 
                     <div class="category-price">
-                        <?php if (empty($item->product->new_price)): ?>
-                            <span class="category-price__new"><?= number_format($item->product->price, 0, '.', ' '); ?> <i class="fa fa-rub" aria-hidden="true"></i></span>
+                        <?php if (empty($item->new_price)): ?>
+                            <span class="category-price__new"><?= number_format($item->price, 0, '.', ' '); ?> <i class="fa fa-rub" aria-hidden="true"></i></span>
                         <?php else:?>
-                            <span class="category-price__old"><?= number_format($item->product->price, 0, '.', ' '); ?> <i class="fa fa-rub" aria-hidden="true"></i></span>
-                            <span class="category-price__new"><?= number_format($item->product->new_price, 0, '.', ' '); ?> <i class="fa fa-rub" aria-hidden="true"></i></span>
+                            <span class="category-price__old"><?= number_format($item->price, 0, '.', ' '); ?> <i class="fa fa-rub" aria-hidden="true"></i></span>
+                            <span class="category-price__new"><?= number_format($item->new_price, 0, '.', ' '); ?> <i class="fa fa-rub" aria-hidden="true"></i></span>
                         <?php endif; ?>
                     </div>
 
