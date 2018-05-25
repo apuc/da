627f6ea6f5049bcc4c0be95fa77ef1435f738229
IgorM0 <vvex@mail.ru>
2018-05-25 11:38:14 +0300

    time limit

--- backend/web/console.php
+++ backend/web/console.php
@@ -3,7 +3,7 @@ echo '<a href="/secure/vk/vk_groups">Назад</a>';
 echo '<br>';
 
 if($_GET['p']=='vk/get-stream'){
-
+    set_time_limit(0);
     // параметры вводимые в консоли, только уже в виде масива
     $_SERVER['argv'] = [ 0=>__FILE__, 1=>'vk/get-stream', 3=>$_GET['id']];
     //if(!empty($_GET['id'])) $_SERVER['argv'][1] .= ' '.$_GET['id'];
@@ -15,7 +15,7 @@ if($_GET['p']=='vk/get-stream'){
 }
 
 if($_GET['p']=='vk/get-group-info'){
-
+    set_time_limit(0);
     // параметры вводимые в консоли, только уже в виде масива
     $_SERVER['argv'] = [ 0=>__FILE__, 1=>'vk/get-group-info'];
     //if(!empty($_GET['id'])) $_SERVER['argv'][1] .= ' '.$_GET['id'];
