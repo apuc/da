8e198dfb0b480dab5c620b553f4e42ac79775adf
IgorM0 <vvex@mail.ru>
2018-06-13 10:02:06 +0300

    fix

--- backend/modules/company/views/company/_form.php
+++ backend/modules/company/views/company/_form.php
@@ -40,7 +40,7 @@ use yii\jui\DatePicker;
 
     <span id="admin_company_category_box">
        <?php
-       if (Yii::$app->controller->action->id === 'update') {
+       if (Yii::$app->controller->action->id === 'update' && isset($model->categories[0])) {
            echo Html::dropDownList(
                'categ',
                $model->categories[0]->parent_id,
