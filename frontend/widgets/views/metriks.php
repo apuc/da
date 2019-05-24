6c0014a7fd2ff4780431eed23a8bbaf6cfd367d5
kavalar <apuc06@mail.ru>
2019-05-24 12:53:59 +0300

    some fix

--- frontend/widgets/views/metriks.php
+++ frontend/widgets/views/metriks.php
@@ -6,45 +6,76 @@
  * Time: 15:33
  */
 ?>
-<?php  if(Yii::$app->request->getUserIP() != '127.0.0.1'):?>
-<!-- Yandex.Metrika counter -->
-<script type="text/javascript">
-    (function (d, w, c) {
-        (w[c] = w[c] || []).push(function() {
-            try {
-                w.yaCounter40198234 = new Ya.Metrika({
-                    id:40198234,
-                    clickmap:true,
-                    trackLinks:true,
-                    accurateTrackBounce:true,
-                    webvisor:true
-                });
-            } catch(e) { }
+<?php if (Yii::$app->request->getUserIP() != '127.0.0.1'): ?>
+    <!— Yandex.Metrika counter —>
+    <script type="text/javascript">
+        (function (m, e, t, r, i, k, a) {
+            m[i] = m[i] || function () {
+                (m[i].a = m[i].a || []).push(arguments)
+            };
+            m[i].l = 1 * new Date();
+            k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+        })
+        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+        ym(53307079, "init", {
+            clickmap: true,
+            trackLinks: true,
+            accurateTrackBounce: true,
+            webvisor: true
         });
+    </script>
+    <noscript>
+        <div><img src="https://mc.yandex.ru/watch/53307079" style="position:absolute; left:-9999px;" alt=""/></div>
+    </noscript>
+    <!— /Yandex.Metrika counter —>
 
-        var n = d.getElementsByTagName("script")[0],
-            s = d.createElement("script"),
-            f = function () { n.parentNode.insertBefore(s, n); };
-        s.type = "text/javascript";
-        s.async = true;
-        s.src = "https://mc.yandex.ru/metrika/watch.js";
+    <script>
+        (function (i, s, o, g, r, a, m) {
+            i['GoogleAnalyticsObject'] = r;
+            i[r] = i[r] || function () {
+                (i[r].q = i[r].q || []).push(arguments)
+            }, i[r].l = 1 * new Date();
+            a = s.createElement(o),
+                m = s.getElementsByTagName(o)[0];
+            a.async = 1;
+            a.src = g;
+            m.parentNode.insertBefore(a, m)
+        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
-        if (w.opera == "[object Opera]") {
-            d.addEventListener("DOMContentLoaded", f, false);
-        } else { f(); }
-    })(document, window, "yandex_metrika_callbacks");
-</script>
-<noscript><div><img src="https://mc.yandex.ru/watch/40198234" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-<!-- /Yandex.Metrika counter -->
+        ga('create', 'UA-85668241-1', 'auto');
+        ga('send', 'pageview');
 
-<script>
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+    </script>
+    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-5402379949049940",
+            enable_page_level_ads: true
+        });
+    </script>
 
-    ga('create', 'UA-85668241-1', 'auto');
-    ga('send', 'pageview');
+    <script type='text/javascript'>
+        (function () {
+            window['yandexChatWidgetCallback'] = function () {
+                try {
+                    window.yandexChatWidget = new Ya.ChatWidget({
+                        guid: '4d63921d-9970-4c04-81d2-1989cb0236cc',
+                        buttonText: 'Ответим на ваши вопросы!',
+                        title: 'Помощник DaInfoPro',
+                        theme: 'dark',
+                        collapsedDesktop: 'never',
+                        collapsedTouch: 'always'
+                    });
+                } catch (e) {
+                }
+            };
+            var n = document.getElementsByTagName('script')[0],
+                s = document.createElement('script');
+            s.async = true;
+            s.src = 'https://chat.s3.yandex.net/widget.js';
+            n.parentNode.insertBefore(s, n);
+        })();
+    </script>
 
-</script>
 <?php endif; ?>
