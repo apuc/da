7cf5b7093865767b3dc28c1aa50716850b2a6fdd
kavalar <apuc06@mail.ru>
2019-05-27 17:35:52 +0300

    remove google ads

--- frontend/widgets/views/metriks.php
+++ frontend/widgets/views/metriks.php
@@ -47,13 +47,13 @@
         ga('send', 'pageview');
 
     </script>
-    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({
-            google_ad_client: "ca-pub-5402379949049940",
-            enable_page_level_ads: true
-        });
-    </script>
+<!--    <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>-->
+<!--    <script>-->
+<!--        (adsbygoogle = window.adsbygoogle || []).push({-->
+<!--            google_ad_client: "ca-pub-5402379949049940",-->
+<!--            enable_page_level_ads: true-->
+<!--        });-->
+<!--    </script>-->
 
     <script type='text/javascript'>
         (function () {
