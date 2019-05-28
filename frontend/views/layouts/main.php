27e4bcfa223380491d173d7c637675af5408ef3a
kavalar <apuc06@mail.ru>
2019-05-28 16:47:20 +0300

    jivo site

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -164,6 +164,16 @@ MainPageAsset::register($this);
 <div id="black-overlay"></div>
 
 <?php $this->endBody() ?>
+<!— BEGIN JIVOSITE CODE {literal} —>
+<script type='text/javascript'>
+    (function(){ var widget_id = 'B0sW1HAj1V';var d=document;var w=window;function l(){
+        var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true;
+        s.src = '//code.jivosite.com/script/widget/'+widget_id
+        ; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}
+        if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}
+        else{w.addEventListener('load',l,false);}}})();
+</script>
+<!— {/literal} END JIVOSITE CODE —>
 </body>
 </html>
 <?php $this->endPage() ?>
