71ab79610d8edd812abcb582ff4a7b32a84fbd23
SoHardKI <tony_millen@mail.ua>
2019-07-01 17:10:38 +0300

    refactor_api

--- frontend/modules/api/controllers/CompanyController.php
+++ frontend/modules/api/controllers/CompanyController.php
@@ -14,19 +14,18 @@ class CompanyController extends \yii\web\Controller
 
     public function actionGetCompanyCategory()
     {
-        $model = CategoryCompany::find()->select(['id','title'])->all();
+        $model = CategoryCompany::find()->select(['id','title']);
         if(\Yii::$app->request->get('parent'))
         {
-            $model = CategoryCompany::find()->select(['id','title'])
-                ->where(['=', 'parent_id', \Yii::$app->request->get('parent')])->all();
+                $model->where(['=', 'parent_id', \Yii::$app->request->get('parent')]);
         }
-
+        $model = $model->all();
         if(!empty($model))
         {
             return $model;
         }
 
-       return 'Нет данных.';
+       return ['Нет данных.'];
 
     }
 }
