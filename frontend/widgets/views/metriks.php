b78dfcb00744e9746cac82ee3c863d6cab37b186
Евсеев Максим <makss.web@gmail.com>
2021-11-02 11:37:08 +0300

    Убрал скрипт рекламы с ДА

--- frontend/widgets/views/metriks.php
+++ frontend/widgets/views/metriks.php
@@ -77,8 +77,6 @@
             n.parentNode.insertBefore(s, n);
         })();
     </script>
-
-    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
     <script>
         (adsbygoogle = window.adsbygoogle || []).push({
             google_ad_client: "ca-pub-7346523585639786",
