9e7d8e31fc996eff3a559336e11454a6fc2c30ff
alex korona <alex.korona.job@gmail.com>
2022-04-27 19:16:02 +0300

    update ViewsForcingController.php

--- console/controllers/ViewsForcingController.php
+++ console/controllers/ViewsForcingController.php
@@ -2,30 +2,44 @@
 
 namespace console\controllers;
 
-
-use backend\modules\news\News;
+use backend\modules\news\models\News;
 use yii\console\Controller;
 
 class ViewsForcingController extends Controller
 {
-    const NEWS_IDS = [1,2,3];
+    const NEWS_IDS = [30334, 30332, 30333];
+
+    public $min, $max;
 
     function init()
     {
         parent::init();
     }
 
-    public function actionIndex()
+    public function options($actionID)
     {
-        print_r('FORCE');
+        $options = parent::options($actionID);
+
+        switch ($actionID) {
+            case 'force-last-day-news':
+                $options[] = 'min';
+                $options[] = 'max';
+                break;
+            default:
+                break;
+        }
+
+        return $options;
     }
 
-    public function actionForce()
+    public function actionForceLastDayNews()
     {
-        foreach (self::NEWS_IDS as $id){
-            $news = News::findOne($id);
-            $news->views = $news->views + rand(2,5);
-            $news->save();
+        // За последние сутки
+        $news = News::find()->where(['>', 'dt_public', time() - 86400])->all();
+
+        foreach ($news as $rec) {
+            $rec->views = $rec->views + rand($this->min, $this->max);
+            $rec->save();
         }
     }
 }
