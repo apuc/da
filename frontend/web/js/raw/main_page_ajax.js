5b386f6a059dbed973507fc264b141723a73624f
alex korona <alex.korona.job@gmail.com>
2022-04-28 16:20:27 +0300

    auto code style

--- frontend/web/js/raw/main_page_ajax.js
+++ frontend/web/js/raw/main_page_ajax.js
@@ -9,14 +9,13 @@ $(document).ready(function () {
 
         var flagMail = 0;
 
-        if(document.getElementById('faq-user-name') && document.getElementById('faq-user-email'))
-        {
+        if (document.getElementById('faq-user-name') && document.getElementById('faq-user-email')) {
             var name = $('#faq-user-name').val();
             var email = $('#faq-user-email').val();
-            if(email != '') {
+            if (email != '') {
                 var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
 
-                if(pattern.test( $('#faq-user-email').val() )){
+                if (pattern.test($('#faq-user-email').val())) {
                     $('.faq-modal-email-error').text('');
                     flagMail = 1;
                 } else {
@@ -30,14 +29,14 @@ $(document).ready(function () {
 
             /*if(!email)
                 $('.faq-modal-email-error').css('color', 'red').text('Введите email');*/
-            if(!name)
+            if (!name)
                 $('.faq-modal-name-error').css('color', 'red').text('Введите Имя');
-        }else {
+        } else {
             var name = 0;
             var email = 0;
         }
         //console.log(name !=='')
-        if(!content) $('.faq-modal-message-error').css('color', 'red').text('Введите сообщение');
+        if (!content) $('.faq-modal-message-error').css('color', 'red').text('Введите сообщение');
 
         if ((content != '') && (name !== '') && (flagMail != 0)) {
 
@@ -49,7 +48,7 @@ $(document).ready(function () {
                     name: name,
                     email: email,
                     _csrf: $('meta[name=csrf-token]').attr("content")
-                } ,
+                },
                 success: function (data) {
                     window.location.reload();
                 }
@@ -59,7 +58,6 @@ $(document).ready(function () {
 
         return false;
     });
-
 
 
 })
