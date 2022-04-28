0f7e361beb1a64321620515de98869b477f2dbad
alex korona <alex.korona.job@gmail.com>
2022-04-28 14:04:05 +0300

    update socials

--- frontend/widgets/views/subscribe.php
+++ frontend/widgets/views/subscribe.php
@@ -20,32 +20,35 @@ $this->registerJsFile('/js/validation.min.js');
             <a href="https://vk.com/da_info_pro" target="_blank" class="social-wrap__item vk">
                 <img src="/theme/portal-donbassa/img/soc/vk.png" alt="">
             </a>
-            <a href="https://www.facebook.com/da.info.pro/" target="_blank" class="social-wrap__item fb">
-                <img src="/theme/portal-donbassa/img/soc/fb.png" alt="">
+            <a href="https://t.me/prosmi_dnr" class="social-wrap__item telegram">
+                <img src="/theme/portal-donbassa/img/soc/telegram-round.png" alt="">
             </a>
-            <a href="https://ok.ru/da...infor" target="_blank" class="social-wrap__item ok">
-                <img src="/theme/portal-donbassa/img/soc/ok-icon.png" alt="">
-            </a>
-            <a href="https://www.instagram.com/da.info.pro/" target="_blank" class="social-wrap__item insta">
-                <img src="/theme/portal-donbassa/img/soc/insta-icon.png" alt="">
-            </a>
-            <a href="https://twitter.com/DA_info_pro" target="_blank" class="social-wrap__item twitter">
-                <img src="/theme/portal-donbassa/img/soc/twi-icon.png" alt="">
-            </a>
-            <a href="https://plus.google.com/u/0/communities/115639152242515279745" target="_blank" class="social-wrap__item google">
-                <img src="/theme/portal-donbassa/img/soc/google-icon.png" alt="">
-            </a>
-            <a href="https://ru.pinterest.com/DA_Info_Pro/da-info-pro/" target="_blank" class="social-wrap__item pinterest">
-                <img src="/theme/portal-donbassa/img/soc/pinter-icon.png" alt="">
-            </a>
-            <a  href="http://da-info-pro.livejournal.com/" target="_blank" class="social-wrap__item live-journal">
-                <img src="/theme/portal-donbassa/img/soc/livejournal-f.png" alt="">
-            </a>
-            <a href="https://www.linkedin.com/in/da-info-pro/recent-activity/" target="_blank" class="social-wrap__item in">
-                <img src="/theme/portal-donbassa/img/soc/in-f.png" alt="">
-            </a>
-            <a href="https://zen.yandex.ru/id/5afbe03e2f578cbf92d410ba" target="_blank" class="social-wrap__item dzen">
-                <img src="/theme/portal-donbassa/img/soc/Yandex_dzen.png" alt="">
+<!--            <a href="https://www.facebook.com/da.info.pro/" target="_blank" class="social-wrap__item fb">-->
+<!--                <img src="/theme/portal-donbassa/img/soc/fb.png" alt="">-->
+<!--            </a>-->
+<!--            <a href="https://ok.ru/da...infor" target="_blank" class="social-wrap__item ok">-->
+<!--                <img src="/theme/portal-donbassa/img/soc/ok-icon.png" alt="">-->
+<!--            </a>-->
+<!--            <a href="https://www.instagram.com/da.info.pro/" target="_blank" class="social-wrap__item insta">-->
+<!--                <img src="/theme/portal-donbassa/img/soc/insta-icon.png" alt="">-->
+<!--            </a>-->
+<!--            <a href="https://twitter.com/DA_info_pro" target="_blank" class="social-wrap__item twitter">-->
+<!--                <img src="/theme/portal-donbassa/img/soc/twi-icon.png" alt="">-->
+<!--            </a>-->
+<!--            <a href="https://plus.google.com/u/0/communities/115639152242515279745" target="_blank" class="social-wrap__item google">-->
+<!--                <img src="/theme/portal-donbassa/img/soc/google-icon.png" alt="">-->
+<!--            </a>-->
+<!--            <a href="https://ru.pinterest.com/DA_Info_Pro/da-info-pro/" target="_blank" class="social-wrap__item pinterest">-->
+<!--                <img src="/theme/portal-donbassa/img/soc/pinter-icon.png" alt="">-->
+<!--            </a>-->
+<!--            <a  href="http://da-info-pro.livejournal.com/" target="_blank" class="social-wrap__item live-journal">-->
+<!--                <img src="/theme/portal-donbassa/img/soc/livejournal-f.png" alt="">-->
+<!--            </a>-->
+<!--            <a href="https://www.linkedin.com/in/da-info-pro/recent-activity/" target="_blank" class="social-wrap__item in">-->
+<!--                <img src="/theme/portal-donbassa/img/soc/in-f.png" alt="">-->
+<!--            </a>-->
+<!--            <a href="https://zen.yandex.ru/id/5afbe03e2f578cbf92d410ba" target="_blank" class="social-wrap__item dzen">-->
+<!--                <img src="/theme/portal-donbassa/img/soc/Yandex_dzen.png" alt="">-->
             </a>
 
         </div>
