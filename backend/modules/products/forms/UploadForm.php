738aad68cfa9e442b3d15855473f0d87f0949b17
alex korona <alex.korona.job@gmail.com>
2022-05-06 12:25:34 +0300

    create UploadForm.php

---
+++ backend/modules/products/forms/UploadForm.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace backend\modules\products\forms;
+
+use yii\base\Model;
+use yii\web\UploadedFile;
+
+class UploadForm extends Model
+{
+    /**
+     * @var UploadedFile
+     */
+    public $imageFile;
+
+    public function rules()
+    {
+        return [
+            [
+                [
+                    'imageFile'
+                ],
+                'file',
+                'skipOnEmpty' => false,
+                'extensions' => 'xls, xlsx'
+            ],
+        ];
+    }
+
+    public function upload()
+    {
+        if ($this->validate()) {
+            $this->imageFile->saveAs('uploads/' . $this->imageFile->baseName . '.' . $this->imageFile->extension);
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
