501f7ef9c52c23ea1741bdf3261d610de41d8560
alex korona <alex.korona.job@gmail.com>
2022-05-11 17:51:17 +0300

    add forced_views model

---
+++ backend/modules/news/models/ForcedView.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace backend\modules\news\models;
+
+use yii\db\ActiveRecord;
+
+class ForcedView extends ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'forced_views';
+    }
+
+    public $newsId;
+    public $views;
+
+    public static function getViews($id): int
+    {
+        $views = static::findOne(['news_id' => $id]);
+
+        return $views->views ?? 30000;
+    }
+
+    function init()
+    {
+        parent::init();
+    }
+}
