36b4b3b10b2f11a1421d9a0ec8a5257f52e808a7
alex korona <alex.korona.job@gmail.com>
2022-05-16 15:17:49 +0300

    remove less js

--- frontend/assets/MainPageAsset.php
+++ frontend/assets/MainPageAsset.php
@@ -25,7 +25,6 @@ class MainPageAsset extends AssetBundle
         '/theme/portal-donbassa/css/fancybox/jquery.fancybox.min.css',
         '/theme/portal-donbassa/css/fancybox/jquery.fancybox-thumbs.min.css',
         '/theme/portal-donbassa/css/fancybox/jquery.fancybox-buttons.min.css',
-//        '/css/lightbox.min.css',
         '/theme/portal-donbassa/css/styles.min.css',
 
     ];
@@ -33,9 +32,6 @@ class MainPageAsset extends AssetBundle
     /** Подключение js по endBody() */
     public $js = [
         'https://api-maps.yandex.ru/2.1/?lang=ru_RU',
-//        '/theme/portal-donbassa/js/countdown.min.js',
-
-//        '/theme/portal-donbassa/js/map.min.js',
         '/theme/portal-donbassa/js/libs.min.js',
         '/theme/portal-donbassa/js/owl.carousel.min.js',
          '/theme/portal-donbassa/js/datepicker.min.js',
@@ -45,13 +41,10 @@ class MainPageAsset extends AssetBundle
         '/theme/portal-donbassa/js/script.min.js',
         '/theme/portal-donbassa/js/raw/ajax.js',
         '/theme/portal-donbassa/js/raw/ajax-modals.js',
-
-//        '/js/poll_ajax.min.js',
         '/js/comments.min.js',
         '/js/share.min.js',
         '/js/main.min.js',
         '/js/main_page_ajax.min.js',
-        '/js/raw/search-missing-person.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
