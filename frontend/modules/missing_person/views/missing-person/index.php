a824e236ce4d6f0f11fe9eb8f1279d818a17d30c
alex korona <alex.korona.job@gmail.com>
2022-05-16 19:32:22 +0300

    update missing-person main view

--- frontend/modules/missing_person/views/missing-person/index.php
+++ frontend/modules/missing_person/views/missing-person/index.php
@@ -1,24 +1,44 @@
 <?php
+
+use common\models\db\GeobaseCity;
+use frontend\modules\mainpage\widgets\ShowSearchMissingPeopleFormForIndex;
+use frontend\modules\missing_person\models\MissingPerson;
+
 /**
  * @var $records
+ * @var $cities GeobaseCity[]
  */
+
+echo ShowSearchMissingPeopleFormForIndex::widget();
 
 ?>
 
-<table>
-    <tr>
-        <th>Имя</th>
-        <th>Дата рождения</th>
-        <th>Город</th>
-    </tr>
-    <?php
-    foreach ($records as $record) { ?>
+<div class="container missing-person__block">
+    <table class="table">
+        <thead>
         <tr>
-            <td><?= $record->FIO ?></td>
-            <td><?= $record->date_of_birth ?></td>
-            <td><?= \common\models\db\GeobaseCity::findOne($record->city_id)->name ?></td>
+            <th>ФИО</th>
+            <th>Дата рождения</th>
+            <th>Город</th>
+            <th>Доп. Информация</th>
         </tr>
-    <?php
-    }
-    ?>
-</table>
+        </thead>
+        <tbody>
+        <?php
+        /**
+         * @var $record MissingPerson
+         */
+        foreach ($records as $record) { ?>
+            <tr>
+                <td><?= $record->FIO ?></td>
+                <td><?= date('d.m.Y', $record->date_of_birth) ?></td>
+                <td><?= GeobaseCity::findOne($record->city_id)->name ?></td>
+                <td><?= strlen($record->additional_info) > 0 ? $record->additional_info : '—' ?></td>
+            </tr>
+            <?php
+        }
+        ?>
+        </tbody>
+    </table>
+
+</div>
