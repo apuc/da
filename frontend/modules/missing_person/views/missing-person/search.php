0c4b7a6345312af3eaf3a3497457cdb108849d24
alex korona <alex.korona.job@gmail.com>
2022-05-23 11:51:16 +0300

    fix fio

--- frontend/modules/missing_person/views/missing-person/search.php
+++ frontend/modules/missing_person/views/missing-person/search.php
@@ -30,7 +30,7 @@ echo ShowSearchMissingPeopleFormForIndex::widget();
             <?php
             foreach ($records as $record) { ?>
                 <tr>
-                    <td><?= $record['FIO'] ?></td>
+                    <td><?= $record['fio'] ?></td>
                     <td><?= date('d.m.Y', $record['date_of_birth']) ?></td>
                     <td><?= GeobaseCity::findOne($record['city_id'])->name ?></td>
                     <td><?= strlen($record['additional_info']) > 0 ? $record['additional_info'] : '—' ?></td>
