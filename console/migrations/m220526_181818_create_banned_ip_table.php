c941b19a85543b98ff98ae6b5285958d307fb079
alex korona <alex.korona.job@gmail.com>
2022-05-26 19:00:28 +0300

    rename typo

--- console/migrations/m220526_181818_create_banned_ip_table.php
+++ console/migrations/m220526_181818_create_banned_ip_table.php
@@ -1,10 +1,10 @@
 <?php
 
-class m220526_181818_create_banned_ips_table extends \yii\db\Migration
+class m220526_181818_create_banned_ip_table extends \yii\db\Migration
 {
     public function up()
     {
-        $this->createTable('banned_ips', [
+        $this->createTable('banned_ip', [
             'id' => $this->primaryKey(),
             'ip_mask' => $this->string(16)->notNull(),
         ]);
