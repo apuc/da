553d4cfa43f4e825e3d85551b48ccf7dbebccc68
alex korona <alex.korona.job@gmail.com>
2022-05-27 14:07:46 +0300

    update BannedIpController.php

--- backend/modules/banned_ip/controllers/BannedIpController.php
+++ backend/modules/banned_ip/controllers/BannedIpController.php
@@ -5,6 +5,7 @@ namespace backend\modules\banned_ip\controllers;
 use backend\modules\banned_ip\models\BannedIp;
 use backend\modules\banned_ip\models\BannedIpSearch;
 use Yii;
+use yii\filters\VerbFilter;
 
 
 class BannedIpController extends \yii\web\Controller
@@ -12,6 +13,25 @@ class BannedIpController extends \yii\web\Controller
     function init()
     {
         parent::init();
+    }
+
+
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'index' => ['GET'],
+                    'create' => ['GET', 'POST'],
+                    'update' => ['GET', 'POST'],
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
     }
 
     public function actionIndex()
@@ -23,5 +43,44 @@ class BannedIpController extends \yii\web\Controller
             'searchModel' => $searchModel,
             'dataProvider' => $dataProvider,
         ]);
+    }
+
+    public function actionCreate()
+    {
+        $model = new BannedIp();
+
+        if ('POST' == Yii::$app->request->method) {
+            $model->setAttribute('ip_mask', Yii::$app->request->getBodyParam('BannedIp')['ip_mask']);
+            $model->save();
+
+            return $this->redirect(['index']);
+        } else {
+            return $this->render('create', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    public function actionUpdate($id)
+    {
+        $model = BannedIp::findById($id);
+
+        if ('POST' == Yii::$app->request->method) {
+            $model->setAttribute('ip_mask', Yii::$app->request->getBodyParam('BannedIp')['ip_mask']);
+            $model->save();
+
+            return $this->redirect(['index']);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    public function actionDelete($id)
+    {
+        BannedIp::findOne($id)->delete();
+
+        return $this->redirect(['index']);
     }
 }
